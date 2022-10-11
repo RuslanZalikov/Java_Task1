bd6c008156b268e309695dbd72d83c61dec25277
ruslanzalikov <rus.zalikov@bk.ru>
2022-10-11 15:27:43 +0300

    second

--- src/task.java
+++ src/task.java
@@ -1,6 +1,6 @@
 public class task {
     public static void main(String[] args){
-        System.out.println(abcmath(42, 5, 10));
+        System.out.println(sumOfCubes(new int[] {1, 2, 3}));
     }
     public static int remainder(int a, int b){return a%b;}
     public static int triArea(int n, int f){return n*f/2;}
@@ -16,6 +16,12 @@ public class task {
     public static int ctoa(char n){return (int) n;}
     public static int addUpTo(int n){return n*(n + 1)/2;}
     public static int nextEdge(int a, int b){return a + b - 1;}
-    public static double sumOfCubes(int a, int b, int c){return Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3);}
+    public static double sumOfCubes(int[] a){
+        int sum = 0;
+        for (int i = 0; i < a.length; i++){
+            sum += Math.pow(a[i], 3);
+        }
+        return sum;
+    }
     public static boolean abcmath(int a, int b, int c){return (a * Math.pow(2, b))%c == 0;}
 }
